5750c7099e8de87b0a40a562163e657a84f02929
AndreySanzharov <Andrsan2004@yandex.ru>
2024-07-23 19:07:29 +0300

    Sprint 6: исправлены старые ошибки, сделаны новые задания

--- src/JavaKanban/Main.java
+++ src/JavaKanban/Main.java
@@ -1,17 +1,52 @@
 package JavaKanban;
 
 import JavaKanban.Elements.Epic;
+import JavaKanban.Elements.Status;
+import JavaKanban.Elements.Subtask;
+import JavaKanban.Elements.Task;
+import JavaKanban.Managers.InMemoryHistoryManager;
 import JavaKanban.Managers.InMemoryTaskManager;
 
 public class Main {
+
+
     public static void main(String[] args) {
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
-        Epic epic = new Epic("Эпик", "Эпик делится на подзадачи");
+        InMemoryHistoryManager inMemoryHistoryManager = new InMemoryHistoryManager();
 
-        inMemoryTaskManager.addEpic(epic);
+        Task task1 = new Task("Задача_1", "Описание_1", Status.NEW);
+        Task task2 = new Task("Задача_2", "Описание_2", Status.IN_PROGRESS);
 
-        System.out.println(inMemoryTaskManager.epicMap);
-        System.out.println(inMemoryTaskManager.getEpicById(0));
+        Subtask sub1 = new Subtask("Подзадача_1", "Подзадача_1", Status.DONE);
+        Subtask sub2 = new Subtask("Подзадача_2", "Подзадача_2", Status.DONE);
+        Subtask sub3 = new Subtask("Подзадача_3", "Подзадача_3", Status.DONE);
+
+        Epic epicWithSub = new Epic("Эпик", "Эпик делится на подзадачи");
+        Epic epicWithoutSub = new Epic("Эпик без подзадач", "Нет подзадач");
+
+        inMemoryTaskManager.addTask(task1);
+        inMemoryTaskManager.addTask(task2);
+        inMemoryTaskManager.addEpic(epicWithSub);
+        inMemoryTaskManager.addEpic(epicWithoutSub);
+        inMemoryTaskManager.addSubtask(2, sub1);
+        inMemoryTaskManager.addSubtask(2, sub2);
+        inMemoryTaskManager.addSubtask(2, sub3);
+
+        inMemoryTaskManager.getTaskById(0);
+        inMemoryTaskManager.getTaskById(1);
+        inMemoryTaskManager.getTaskById(1);
+        inMemoryTaskManager.getTaskById(1);
+
+        System.out.println(InMemoryHistoryManager.history);
+
+        inMemoryTaskManager.deleteTaskById(0);
+        inMemoryTaskManager.deleteTaskById(1);
+        System.out.println(InMemoryHistoryManager.history);
+
+        inMemoryTaskManager.getEpicById(2);
+        System.out.println(InMemoryHistoryManager.history);
+
+        inMemoryTaskManager.deleteEpicById(2);
 
     }
 }
--- src/JavaKanban/Managers/InMemoryHistoryManager.java
+++ src/JavaKanban/Managers/InMemoryHistoryManager.java
@@ -15,22 +15,22 @@ public class InMemoryHistoryManager implements HistoryManager {
     public static Map<Integer, Node<Task>> history = new HashMap();
 
     private void linklast(Task task) {
-       Node<Task> newNode = new Node<>(task);
-       if (tail == null){
-           head = newNode;
-           tail = newNode;
-       }else{
-           tail.next = newNode;
-           newNode.prev = tail;
-           tail = newNode;
-       }
-       history.put(task.getId(), newNode);
+        Node<Task> newNode = new Node<>(task);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        history.put(task.getId(), newNode);
     }
 
     private List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
         Node<Task> current = head;
-        while (current != null){
+        while (current != null) {
             tasks.add(current.data);
             current = current.next;
         }
@@ -38,15 +38,15 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     private void removeNode(Node<Task> node) {
-        if (node.prev != null){
+        if (node.prev != null) {
             node.prev.next = node.next;
-        }else{
+        } else {
             head = node.next;
         }
 
-        if (node.next != null){
+        if (node.next != null) {
             node.next.prev = node.prev;
-        }else{
+        } else {
             tail = node.prev;
         }
 
@@ -55,11 +55,11 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-       Node<Task> existingNode = history.get(task.getId());
-       if (existingNode != null){
-           removeNode(existingNode);
-       }
-       linklast(task);
+        Node<Task> existingNode = history.get(task.getId());
+        if (existingNode != null) {
+            removeNode(existingNode);
+        }
+        linklast(task);
     }
 
     @Override
@@ -72,7 +72,6 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         return getTasks();
     }
-
 
 }
 
